f3f2c1a6eca8500e224459f164415ec756d4b612
liosharl <144350294+liosharl@users.noreply.github.com>
2023-09-07 16:29:46 +0300

    jurvers one .py

--- jurvers one .py
+++ jurvers one .py
@@ -4,23 +4,23 @@
 pairs = [
     [
         r"hi|hello",
-        ["whats up dammy", "Hi dammy", "Hey!dammy"]
+        ["whats up ", "Hi there ", "Hey!"]
     ],
     [
         r"how are you|how's it going",
-        ["what the f**k do you think i am ,I'm just a chatbot, but I'm here to help!", "I don't have feelings, but f**k 0ff for asking!"]
+        ["am sorry i dont understand feelings  ,I'm just a chatbot, but I'm here to help!", "I don't have feelings, but thanks for asking for asking!"]
     ],
     [
         r"what is your name|who are you",
-        ["your mama,I'm a chatbot called ChatGPT.", "You can call me ChatGPT."]
+        [",I'm a chatbot called Y.V.O.N.E", "You can call me Yvone"]
     ],
     [
         r"bye|goodbye",
-        ["f**k off ", "Take care!", "die soon !"]
+        ["goodbye toohave a nice time  ", "Take care!", "see you soon !"]
     ],
     [
         r"(.*)",
-        ["sorry dammy, I don't understand that.", "Could you please rephrase that?", "I'm still learning."]
+        ["sorry , I don't understand that.", "Could you please rephrase that?", "I'm still learning."]
     ]
 ]
 
@@ -31,10 +31,10 @@ def chatbot():
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'exit':
-            print("Chatbot: Goodbye!")
+            print("Y.V.O.N.E : Goodbye!")
             break
         response = chat.respond(user_input)
-        print("Chatbot:", response)
+        print("Y.V.O.N.E :", response)
 
 if __name__ == "__main__":
     chatbot()
